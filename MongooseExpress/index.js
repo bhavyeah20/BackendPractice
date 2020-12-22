862691f6409860b00572b8008cce9364ee88c200
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2020-12-22 18:26:55 +0530

    Pushed all code from MongooseExpress

---
+++ MongooseExpress/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+const methodOverride = require('method-override')
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("we're in")
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
+
+
+
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`products/${newProduct._id}`);
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
---
+++ MongooseExpress/models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy']
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
